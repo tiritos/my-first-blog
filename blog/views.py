4737ed674205436c842f5444fb0d5c64b22199d2
tiritos <fachahansen@gmail.com>
2018-10-25 23:47:40 -0300

    boton borrar agregado

--- blog/views.py
+++ blog/views.py
@@ -41,3 +41,14 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post.delete()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_delete.html', {'form': form})
